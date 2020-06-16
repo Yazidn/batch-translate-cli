f6504f6ef671705aff40007f27c98bd45eb382ff
Yazid <secho771@gmail.com>
2020-06-16 14:57:06 +0100

    For a basic app. it's ready. i'm thinking of adding more options.

--- codename.ts
+++ codename.ts
@@ -1,5 +1,15 @@
+import Spinner from 'https://raw.githubusercontent.com/ameerthehacker/cli-spinners/master/mod.ts';
+const spinner = Spinner.getInstance();
+
 import { parse } from "https://deno.land/std/flags/mod.ts";
 const flags = parse(Deno.args);
+
+import { jsonTree } from "https://deno.land/x/json_tree/mod.ts";
+import { open } from "https://raw.githubusercontent.com/denjucks/opener/master/mod.ts";
+
+
+
+
 const key = 'trnsl.1.1.20200413T163250Z.6743358c267906df.310e0f5e8f0e75528cdf34c2a7040d6a4bcbec64';
 
 if (flags.t || flags.translate) getSupportedLanguages(flags.t || flags.translate, key);
@@ -7,7 +17,8 @@ if (flags.h || flags.help) help();
 
 async function getSupportedLanguages(flag: any, key: string) {
     const supportedLanguages :any[] = [];
-    console.log('Preparing..');
+    spinner.setSpinnerType('moon');
+    spinner.start('Preparing..');
     const response = await fetch(`https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=${key}&ui=en`);
     const data = await response.json();
     let [keys, values] = [Object.keys(data.langs), Object.values(data.langs)]
@@ -16,15 +27,39 @@ async function getSupportedLanguages(flag: any, key: string) {
 }
 
 async function translate(supportedLanguages: any, flag:any, key: string) {
-    const urls :string[] = supportedLanguages.map((language :any) => `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${flag}&lang=${language.code}`);
-    console.log('Translating..');
 
-    Promise.all(urls.map(u =>fetch(u))).then(responses =>
-        Promise.all(responses.map(res => res.json()))
-    ).then(data => {
-        console.table(data);
-        console.log('Success!');
+    const reg_ex = /([\w])/g;
+
+    const translations :any[] = [];
+    let fmt_translations :any[] = [];
+    const urls :string[] = supportedLanguages.map((language :any) => `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${flag}&lang=${language.code}`);
+    
+    spinner.setText('Translating. Hold tight, good things take time!');
+
+    for (let index = 0; index < urls.length; index++) {
+        const req = await fetch(urls[index]);
+        const res = await req.json();
+        
+        if (flags.a || flags.all) translations.push(res);
+        else {
+            if (reg_ex.exec(res.text[0])) translations.push(res);
+        }
+        
+    }
+
+    fmt_translations = translations.map(t => {
+        return {
+            [t.text[0]]: t.text[0],
+            [t.lang]: t.lang,
+            'Powered by Yandex.Translate': '',
+        }
     })
+
+    spinner.stop();
+    console.log(jsonTree(fmt_translations, false));
+    
+    if (!flags.b && !flags.browser) await open("http://translate.yandex.com");
+
 }
 
 async function help() { console.log(await Deno.readTextFile('./help')) }
